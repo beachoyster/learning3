7c52ffc2e2022b56d0abf273a98a6391119ddb8b
rheophile10 <rheophile10@gmail.com>
2020-02-13 15:29:11 -0500

    argv stuff

---
+++ argv.py
@@ -0,0 +1,44 @@
+#with python, you can create variables in one file
+# and you can bring them into the file you're working in
+
+#make a .py file and put a variable in there called x and give it a value
+#import that variable into this file like this
+#from %s import x   where %s is whatever file you created
+#print x
+
+
+#When you installed python, you got all lot of python software other people
+#wrote
+#You can use this software by importinging it
+#when you import a file, python looks at the directory you're in
+# and the directory all your python libraries are saved
+
+
+# uncomment the code below
+
+# from sys import argv
+
+# script, user_name, first, second, third = argv
+
+#print("The script is called:", script)
+#print("Your computer user name is:" user_name)
+#print("Your first variable is:", first)
+#print("Your second variable is:", second)
+#print("Your third variable is:", third)
+
+#when you run this program in terminal type python argv.py ian is awesome
+
+
+# what happened?
+
+
+#sys lets you access global variables in your operating system
+# like user_name as well as letting your user enter additional variables
+# use argv to make a program that will let a user input what kind of girl
+# honey is at terminal and then print it when the program runs
+# I want to run the program from terminal like this
+# "python yourprogram.py good"
+
+
+
+
