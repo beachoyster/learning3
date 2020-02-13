55c61e2617dc07b497274f8c37c6ffd0fd6fd098
beachoyster <ranliuca@gmail.com>
2020-02-13 13:04:59 -0500

    test

--- exercise.py
+++ exercise.py
@@ -2,3 +2,8 @@
 
 print("i could have code like this")
 print("this will run")
+
+parents, babies = (1, 1)
+while babies < 100:
+    print('this generation has {0} babies' . format(babies))
+    parents, babies = (babies, parents + babies)
