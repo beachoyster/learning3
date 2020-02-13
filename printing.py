6bb13fabc7c2c9467489befa5c1413fac166e37c
rheophile10 <rheophile10@gmail.com>
2020-02-13 15:07:02 -0500

    Everything you ever wanted to know about strings (more in fact)

---
+++ printing.py
@@ -0,0 +1,88 @@
+#uncomment the code below and run it
+
+# x = input("what kind of girl is honey?")
+# y = "Honey is a %s girl!" % x
+#print(y)
+
+# y is a formatted string.
+
+# make some more formatted strings.
+#   %d is for numbers
+#   %r shows data in its "original form" - try it
+
+
+
+
+# Single and double quotes denote strings.
+# How can you print a quote sign?
+# you use escapes
+# escapes are like this \
+
+# print('\'')
+
+# print Honey says "woof"!
+
+# we define strings with single line functions
+# What if you want to have a new line?
+# you can use this kind of escape \n
+
+#print ('look at \n this')
+
+# print Honey
+#        eats
+#        oranges
+
+
+# another way to show strings with multiple lines is to use triple quotes
+# like this:
+
+x = """ Once upon a time
+I ate a whole frog
+And it was entirely delicious"""
+
+# print x
+
+
+# make your own multi-line string and print it
+
+
+
+# what if you want to print a tab?
+# guess what, you can do it with \t
+# print a tabbed string below
+
+
+
+# but wait! if escapes (\) escape things, what do you do if you want
+# to print a backslash?
+# no worries, it's like escaping a quote (\")
+# you can go "\\"
+# print a bunch of backslashes below
+
+
+
+
+# It's time to get raw input from people
+# input("some question you want to ask: ") will print the string inside it
+# and then let the user enter data from the console
+
+#uncomment the below:
+
+#x = input('What\'s up? ')
+#print(x)
+
+#write me a python program (in a separate py file) that asks for my age,
+# weight, and how smelly I am
+# make it print my responses
+
+
+
+
+
+
+
+
+
+
+
+
