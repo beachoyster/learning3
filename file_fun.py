3432f2c35961707d4b009c0a1d9ce3627f22d967
rheophile10 <rheophile10@gmail.com>
2020-02-13 20:55:21 -0500

    File fun!

---
+++ file_fun.py
@@ -0,0 +1,72 @@
+#You will often hear nerds talking about io - input/output
+#Your keyboard is an input, your mouse is an input, and you get
+# output on screen and into saved files
+
+#we'll do some input and output stuff with pyhton now
+
+#uncomment the script below, run it and tell me what it does
+
+#from sys import argv
+
+#script, filename = argv
+
+#print("opening the file")
+#file = open(filename, 'w')
+
+#print("Truncating the file! (Erasing everything")
+#file.truncate()
+
+#print("now I'm going to ask you to enter data into the file")
+
+#line1 = input("line 1: ")
+#line2 = input("line 2: ")
+#line3 = input("line 3: ")
+
+#print("I'm going to write these two the file")
+
+#file.write(line1 + '\n')
+#file.write(line2 + '\n')
+#file.write(line3 + '\n')
+
+#print("now we're closing the file")
+
+#file.close()
+
+#print("now we're going to re-open the file")
+
+#file = open(filename, 'r')
+
+#print("now we'll read the file")
+
+#file_content = file.read()
+
+#print(file_content)
+
+# notice that when we open the file, we either open it to read or we open
+# it to write. Look at the tomes we used the open() function in the program
+# above. When did we use it to open the file for writing and when did we
+# use it to open the file for reading? How did we signify to python that
+# we were reading or writing?
+
+
+# Have you ever copied and pasted a file from one folder to another?
+# Let's write a program to do that
+
+
+# in a new python file, write a program that
+#   opens an existing file,
+#   copies content of that file,
+#   opens a new file
+#   writes the content to the new file
+#   and closes both files
+
+#the program should also ask the user for the name of
+#   the file they want to copy
+#   the name of the new file they want to copy to
+
+#
+
+
+
+
+
